8ee39fbea0d90a2b4964e8038f032396b38aecff
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-08-13 21:56:38 +1000

    Add button to fix the display issue of the delete button

--- script.js
+++ script.js
@@ -9,8 +9,15 @@ workoutForm.addEventListener('submit', function (e) {
   const duration = document.getElementById('duration').value;
 
   const entry = document.createElement('li');
-  entry.innerHTML = `<strong>${date}</strong> - ${exercise} (${duration} mins)`;
+  entry.innerHTML = `<strong>${date}</strong> - ${exercise} (${duration} mins) <button class="delete-button">Delete</button>`;
   entriesList.appendChild(entry);
 
   workoutForm.reset();
+});
+
+entriesList.addEventListener('click', function (e) {
+  if (e.target.classList.contains('delete-button')) {
+    const entry = e.target.parentElement;
+    entriesList.removeChild(entry);
+  }
 });

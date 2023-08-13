be95da146f3f9fffa216ac7462c3c475ef540e9b
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-08-13 22:00:09 +1000

    Add js code to make it so On page load, the existing workout entries are retrieved from local storage and displayed in the list. When a new entry is added, it's saved to local storage as a JSON object. When an entry is deleted, it's removed from the list and also removed from the local storage using the saved entry's HTML content as a reference.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const workoutForm = document.getElementById('workoutForm');
 const entriesList = document.getElementById('entriesList');
 
+// Load existing entries from local storage on page load
+document.addEventListener('DOMContentLoaded', function () {
+  const savedEntries = JSON.parse(localStorage.getItem('workoutEntries')) || [];
+
+  savedEntries.forEach(entry => {
+    const entryElement = document.createElement('li');
+    entryElement.innerHTML = `${entry.html}<button class="delete-button">Delete</button>`;
+    entriesList.appendChild(entryElement);
+  });
+});
+
 workoutForm.addEventListener('submit', function (e) {
   e.preventDefault();
 
@@ -8,9 +19,14 @@ workoutForm.addEventListener('submit', function (e) {
   const exercise = document.getElementById('exercise').value;
   const duration = document.getElementById('duration').value;
 
-  const entry = document.createElement('li');
-  entry.innerHTML = `<strong>${date}</strong> - ${exercise} (${duration} mins) <button class="delete-button">Delete</button>`;
-  entriesList.appendChild(entry);
+  const entryElement = document.createElement('li');
+  entryElement.innerHTML = `<strong>${date}</strong> - ${exercise} (${duration} mins) <button class="delete-button">Delete</button>`;
+  entriesList.appendChild(entryElement);
+
+  // Save the new entry to local storage
+  const savedEntries = JSON.parse(localStorage.getItem('workoutEntries')) || [];
+  savedEntries.push({ html: entryElement.innerHTML });
+  localStorage.setItem('workoutEntries', JSON.stringify(savedEntries));
 
   workoutForm.reset();
 });
@@ -19,5 +35,14 @@ entriesList.addEventListener('click', function (e) {
   if (e.target.classList.contains('delete-button')) {
     const entry = e.target.parentElement;
     entriesList.removeChild(entry);
+
+    // Remove the deleted entry from local storage
+    const savedEntries = JSON.parse(localStorage.getItem('workoutEntries')) || [];
+    const entryIndex = savedEntries.findIndex(savedEntry => savedEntry.html === entry.innerHTML);
+    if (entryIndex !== -1) {
+      savedEntries.splice(entryIndex, 1);
+      localStorage.setItem('workoutEntries', JSON.stringify(savedEntries));
+    }
   }
 });
+
